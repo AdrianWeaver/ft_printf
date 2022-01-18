367b5cc76d942427c525fa6651ad926262ae0267
aweaver <aweaver@student.42.fr>
2022-01-18 15:48:56 +0100

    working on compilation issues for test rule on makefile

---
+++ sources/ft_printf_s.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_s.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 14:59:49 by aweaver           #+#    #+#             */
+/*   Updated: 2022/01/18 15:37:08 by aweaver          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+#include "libft.h"
+
+void	ft_printf_s(char *str, t_list_printf *list)
+{
+	int	ret;
+
+	ret = 0;
+	if (list->flag_precision == 1)
+	{
+		while (*str && list->precision_width != 0)
+		{
+			ret += ft_putchar(*str);
+			list->precision_width--;
+			str++;
+		}
+	}
+	else if (list->flag_precision == 0 && list->flag_hyphen == 0)
+		ret += ft_putstr(str);
+	if (list->flag_hyphen == 1)
+	{
+		while (list->width != 0)
+		{
+			ret += ft_putchar(' ');
+			list->width--;
+		}
+	}
+}
