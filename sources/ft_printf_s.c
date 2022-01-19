92dab9ee25653964e265150d0c815b2e73b8d98c
Adrian Weaver <aweaver@student.42.fr>
2022-01-19 09:01:34 +0100

    adding working solution for s convertion

--- sources/ft_printf_s.c
+++ sources/ft_printf_s.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 14:59:49 by aweaver           #+#    #+#             */
-/*   Updated: 2022/01/18 15:37:08 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/01/19 08:56:44 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,24 @@ void	ft_printf_s(char *str, t_list_printf *list)
 	ret = 0;
 	if (list->flag_precision == 1)
 	{
-		while (*str && list->precision_width != 0)
+		while (*str && list->precision_width > 0)
 		{
 			ret += ft_putchar(*str);
 			list->precision_width--;
 			str++;
 		}
 	}
-	else if (list->flag_precision == 0 && list->flag_hyphen == 0)
+	else if (list->flag_precision == 0)
 		ret += ft_putstr(str);
+	list->width -= ret;
 	if (list->flag_hyphen == 1)
 	{
-		while (list->width != 0)
+		while (list->width > 0)
 		{
 			ret += ft_putchar(' ');
 			list->width--;
 		}
 	}
+	list->i++;
+	list->ret += ret;
 }
