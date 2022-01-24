9dc08713466493194009e67bac72437080b1325a
Adrian Weaver <aweaver@student.42.fr>
2022-01-24 17:40:57 +0100

    WIP x conversion

--- sources/ft_printf_x.c
+++ sources/ft_printf_x.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 16:57:00 by aweaver           #+#    #+#             */
-/*   Updated: 2022/01/24 17:27:15 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/01/24 17:37:29 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,30 +89,21 @@ static char	*x_flag_precision(char *tmp, t_list_printf *list,
 	return (str);
 }
 
-static void	x_flag_hyphen(t_list_printf *list)
-{
-	if (list->flag_hyphen == 1)
-	{
-		while (list->width > 0)
-		{
-			list->ret += ft_putchar(' ');
-			list->width--;
-		}
-	}
-}
-
 void	ft_printf_x(unsigned int unbr, t_list_printf *list, const char *src)
 {
 	char	*str;
 
+	if (src[list->i] == 'x')
 	str = ft_uitoa_base((size_t)unbr, 16, "0123456789abcdef");
+	else
+	str = ft_uitoa_base((size_t)unbr, 16, "0123456789ABCDEF");
 	str = x_flag_hashtag(src, list, str);
 	str = x_flag_precision(str, list, unbr);
 	x_nohyphen_flag(str, list);
 	x_make_magic(list, unbr);
 	list->ret += ft_putstr(str);
 	list->width -= ft_strlen(str);
-	x_flag_hyphen(list);
+	ft_flag_hyphen(list);
 	list->i++;
 	free(str);
 }
