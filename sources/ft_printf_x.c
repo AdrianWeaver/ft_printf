ced0f531339fbb671691ba989171e17989bca871
Adrian Weaver <aweaver@student.42.fr>
2022-01-26 10:26:40 +0100

    working solution for x conversion but 0 utoa_base needs fixing

--- sources/ft_printf_x.c
+++ sources/ft_printf_x.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 16:57:00 by aweaver           #+#    #+#             */
-/*   Updated: 2022/01/25 17:17:26 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/01/26 10:20:35 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,87 +14,71 @@
 #include "libft.h"
 #include <stdio.h>
 
-static char	*x_flag_hashtag(const char *src, t_list_printf *list, char *str)
-{
-	char	*ret;
-	char	*tmp;
-
-	if (list->flag_hashtag == 1)
-	{
-		if (list->flag_precision == 1 && list->precision_width >= (int)ft_strlen(str))
-			list->width += 2;
-		tmp = malloc(sizeof(*tmp) * 3);
-		tmp[0] = '0';
-		tmp[1] = src[list->i];
-		tmp[2] = '\0';
-		ret = ft_strjoin(tmp, str);
-		list->width -= 2;
-		free(str);
-		free(tmp);
-		return (ret);
-	}
-	return (str);
-}
-
-static void	x_make_magic(t_list_printf *list, unsigned int nbr)
-{
-	while ((list->flag_precision == 1 && list->precision_width == 0
-			&& list->flag_zero == 0 && list->flag_hyphen == 0
-			&& nbr == 0) && (list->width > 0))
-	{
-		list->ret += ft_putchar(' ');
-		list->width--;
-	}
-}
-
-static void	x_nohyphen_flag(char *str, t_list_printf *list)
+static void	x_noflag(char *str, t_list_printf *list)
 {
 	int	str_len;
 
-	if (list->flag_hyphen == 0 && list->flag_zero == 0 && list->width > 1)
+	str_len = ft_strlen_int(str);
+	if (list->flag_hyphen == 0 && list->flag_zero == 0)
 	{
-		str_len = ft_strlen(str);
 		while (list->width > str_len)
 		{
 			list->ret += ft_putchar(' ');
 			list->width--;
 		}
 	}
-	if (list->flag_hyphen == 0 && list->flag_zero == 1 && list->width > 1)
-	{
-		str_len = ft_strlen(str);
-		while (list->width > str_len)
-		{
-			list->ret += ft_putchar('0');
-			list->width--;
-		}
-	}
 }
 
-static char	*x_flag_precision(char *str, t_list_printf *list,
-		unsigned int unbr)
+static char	*x_hashtag(const char *src, char *str, t_list_printf *list)
 {
 	char	*tmp;
-	int		str_len;
+	char	*dummy;
 
-	str_len = ft_strlen(str);
-	if (list->flag_precision == 1 && list->precision_width == 0 && unbr == 0)
+	if (list->flag_hashtag == 1)
 	{
-		*str = 0;
-		return (str);
+		dummy = malloc(sizeof(*dummy) * 3);
+		dummy[0] = '0';
+		dummy[1] = src[list->i];
+		dummy[2] = 0;
+		tmp = ft_strjoin(dummy, str);
+		free(str);
+		str = tmp;
 	}
-	if (list->precision_width < 0)
-		list->flag_precision = 0;
-	if (list->flag_precision == 1)
+	return (str);
+}
+
+static char	*x_precision(const char *src, char *str, t_list_printf *list)
+{
+	int		str_len;
+	char	*tmp;
+	char	*dummy;
+
+	str_len = ft_strlen_int(str);
+	dummy = ft_strdup("0");
+	if (list->flag_zero == 1 && list->flag_precision != 1)
 	{
-		while (list->precision_width > str_len)
+		if (list->flag_hashtag == 1)
+			str_len += 2;
+		while (str_len < list->width)
 		{
-			tmp = ft_strjoin("0", str);
-			list->precision_width--;
+			tmp = ft_strjoin(dummy, str);
 			free(str);
 			str = tmp;
+			str_len++;
 		}
 	}
+	if (list->flag_precision == 1)
+	{
+		while (str_len < list->precision_width)
+		{
+			tmp = ft_strjoin(dummy, str);
+			free(str);
+			str = tmp;
+			str_len++;
+		}
+	}
+	free(dummy);
+	str = x_hashtag(src, str, list);
 	return (str);
 }
 
@@ -106,12 +90,11 @@ void	ft_printf_x(unsigned int unbr, t_list_printf *list, const char *src)
 		str = ft_utoa_base(unbr, 16, "0123456789abcdef");
 	else
 		str = ft_utoa_base(unbr, 16, "0123456789ABCDEF");
-	str = x_flag_hashtag(src, list, str);
-	str = x_flag_precision(str, list, unbr);
-	x_nohyphen_flag(str, list);
-	x_make_magic(list, unbr);
+	ft_printf_check_flags(list);
+	str = x_precision(src, str, list);
+	x_noflag(str, list);
 	list->ret += ft_putstr(str);
-	list->width -= ft_strlen(str);
+	list->width -= ft_strlen_int(str);
 	ft_flag_hyphen(list);
 	list->i++;
 	free(str);
