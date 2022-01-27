1afbcf6fa8c5bb3b112021d3dfc21c1cd50ff918
Adrian Weaver <aweaver@student.42.fr>
2022-01-27 10:05:40 +0100

    Working solution for all conversions with all flags needs norminetting

--- sources/ft_printf_id.c
+++ sources/ft_printf_id.c
@@ -6,14 +6,25 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 14:12:49 by aweaver           #+#    #+#             */
-/*   Updated: 2022/01/26 15:57:56 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/01/27 10:04:10 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include "libft.h"
 
-static void	u_make_magic(t_list_printf *list, unsigned int nbr)
+static void	id_flag_plus_space(t_list_printf *list, int nbr)
+{
+	if (list->flag_plus == 1 && nbr >= 0)
+		list->ret += ft_putchar('+');
+	if (list->flag_space == 1 && !(nbr < 0))
+	{
+		list->ret += ft_putchar(' ');
+		list->width--;
+	}
+}
+
+static void	id_make_magic(t_list_printf *list, int nbr)
 {
 	while ((list->flag_precision == 1 && list->precision_width == 0
 			&& list->flag_zero == 0 && list->flag_hyphen == 0
@@ -24,14 +35,13 @@ static void	u_make_magic(t_list_printf *list, unsigned int nbr)
 	}
 }
 
-static void	u_nohyphen_flag(char *str, t_list_printf *list)
+static char	*id_nohyphen_flag(char *str, t_list_printf *list, int nbr)
 {
-	int	str_len;
+	char	*tmp;
 
 	if (list->flag_hyphen == 0 && list->flag_zero == 0 && list->width > 1)
 	{
-		str_len = ft_strlen_int(str);
-		while (list->width > str_len)
+		while (list->width > ft_strlen_int(str))
 		{
 			list->ret += ft_putchar(' ');
 			list->width--;
@@ -39,41 +49,47 @@ static void	u_nohyphen_flag(char *str, t_list_printf *list)
 	}
 	if (list->flag_hyphen == 0 && list->flag_zero == 1 && list->width > 1)
 	{
-		str_len = ft_strlen_int(str);
-		while (list->width > str_len)
+		while (list->width > ft_strlen_int(str))
 		{
-			list->ret += ft_putchar('0');
-			list->width--;
+			if (nbr < 0)
+				tmp = ft_strjoin("-0", &str[1]);
+			else
+				tmp = ft_strjoin("0", str);
+			free(str);
+			str = tmp;
 		}
 	}
+	return (str);
 }
 
-static char	*id_flag_precision(char *tmp, t_list_printf *list, int nbr)
+static char	*id_flag_precision(char *str, t_list_printf *list, int nbr)
 {
-	char	*str;
-	int		str_len;
+	char	*tmp;
 
-	str_len = ft_strlen_int(tmp);
 	if (list->precision_width < 0)
 		list->flag_precision = 0;
 	if (list->flag_precision == 1 && list->precision_width == 0 && nbr == 0)
 	{
-		free(tmp);
-		tmp = malloc(sizeof(*tmp) * 1);
-		*tmp = 0;
-		return (tmp);
+		free(str);
+		str = malloc(sizeof(*str) * 1);
+		*str = 0;
+		return (str);
 	}
-	if (list->flag_precision == 1)
+	while (list->flag_precision == 1 && nbr < 0
+		&& list->precision_width >= ft_strlen_int(str))
 	{
-		while (list->precision_width > str_len)
-		{
-			str = ft_strjoin("0", tmp);
-			list->precision_width--;
-			free(tmp);
-			tmp = str;
-		}
+		tmp = ft_strjoin("-0", &str[1]);
+		free(str);
+		str = tmp;
 	}
-	return (tmp);
+	while (list->flag_precision == 1 && nbr >= 0
+		&& (list->precision_width > ft_strlen_int(str)))
+	{
+		tmp = ft_strjoin("0", str);
+		free(str);
+		str = tmp;
+	}
+	return (str);
 }
 
 void	ft_printf_id(int nbr, t_list_printf *list)
@@ -81,9 +97,10 @@ void	ft_printf_id(int nbr, t_list_printf *list)
 	char	*str;
 
 	str = ft_itoa(nbr);
+	id_flag_plus_space(list, nbr);
 	str = id_flag_precision(str, list, nbr);
-	u_nohyphen_flag(str, list);
-	u_make_magic(list, nbr);
+	str = id_nohyphen_flag(str, list, nbr);
+	id_make_magic(list, nbr);
 	list->ret += ft_putstr(str);
 	list->width -= ft_strlen_int(str);
 	ft_flag_hyphen(list);
