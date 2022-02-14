5afbd71707ccfb4d17d5ebc9b17abdc51f86c05d
Adrian WEAVER <aweaver@student.42.fr>
2022-02-14 11:13:55 +0100

    changing the way the parsing was dealt with to properly recognize star on precision

--- sources/ft_printf_parse.c
+++ sources/ft_printf_parse.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 11:25:07 by aweaver           #+#    #+#             */
-/*   Updated: 2022/02/08 10:28:49 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/02/14 11:10:49 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 
 static void	ft_printf_parseloop(const char *str, t_list_printf *list)
 {
-	while (str[list->i] == '0' || str[list->i] == '-' || str[list->i] == '+'
-		|| str[list->i] == ' ' || str[list->i] == '#')
+	while (ft_strchr(FLAG, str[list->i]))
 	{
 		if (str[list->i] == '0')
 			list->flag_zero = 1;
@@ -33,14 +32,14 @@ static void	ft_printf_parseloop(const char *str, t_list_printf *list)
 }
 
 static void	ft_printf_check_star(const char *str, t_list_printf *list,
-		va_list params)
+		va_list params, int precision)
 {
-	if (str[list->i] == '*' && list->flag_precision == 0)
+	if (str[list->i] == '*' && precision == 0)
 	{
 		list->width = va_arg(params, int);
 		list->i++;
 	}
-	else if (str[list->i] == '*' && list->flag_precision == 1)
+	else if (str[list->i] == '*' && precision == 1)
 	{
 		list->precision_width = va_arg(params, int);
 		list->i++;
@@ -50,10 +49,10 @@ static void	ft_printf_check_star(const char *str, t_list_printf *list,
 void	ft_printf_parse(const char *str, t_list_printf *list, va_list params)
 {
 	ft_lstreset(list);
-	while (str[list->i] && ft_strchr(CONVERSION, str[list->i]) == 0)
+	if (str[list->i] && ft_strchr(CONVERSION, str[list->i]) == 0)
 	{
 		ft_printf_parseloop(str, list);
-		ft_printf_check_star(str, list, params);
+		ft_printf_check_star(str, list, params, 0);
 		if (ft_isdigit(str[list->i]) == 1 && list->flag_precision == 0)
 		{
 			list->width = ft_atoi(&str[list->i]);
@@ -69,7 +68,8 @@ void	ft_printf_parse(const char *str, t_list_printf *list, va_list params)
 			list->precision_width = ft_atoi(&str[list->i]);
 			while (ft_isdigit(str[list->i]) == 1)
 				list->i++;
-		}	
+			ft_printf_check_star(str, list, params, 1);
+		}
 	}
 	ft_printf_check_flags(list);
 }
